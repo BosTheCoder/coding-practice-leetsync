76a392728639dc09b61bf608ebe0f8f1cc51fdaa
Bosire Nyakundi <25325707+BossCodersHQ@users.noreply.github.com>
2024-12-08 12:34:08 +0000

    Time: 7 ms (53.39%) | Memory: 18 MB (28.68%) - LeetSync

--- 338-counting-bits/counting-bits.py
+++ 338-counting-bits/counting-bits.py
@@ -1,12 +1,28 @@
 class Solution:
     def countBits(self, n: int) -> List[int]:
-        def count1s(s: str)->int:
-            count = 0
-            for c in s:
-                if c == "1":
-                    count+=1
-            return count
-        ret = [0] * (n+1)
-        for i in range(n+1):
-            ret[i]=count1s(f"{i:b}")
-        return ret
+        dp = [0] * (n+1)
+        marker = 1
+        for i in range(1, n+1):
+            if i == marker*2:
+                marker *= 2
+            # print(i, marker, dp)
+            dp[i] = 1 + dp[i-marker]
+            
+        return dp
+
+"""
+maker = 2
+when i = marker, marker *= 2
+
+0: 0000 0
+1: 0001 = 1 + dp[1-1]
+2: 0010 = 1 + dp[2-2] = 1
+3: 0011 = 1 + dp[3-2] = 1+1 = 2
+4: 0100 = 1 + dp[4-4] = 0
+5: 0101 = 1 + dp[5-4] = 2
+6: 0110 = 1 + dp[6-4] = 2
+7: 0111 = 1 + dp[7-4] = 3
+8: 1000 = 1 + dp[8-8] = 1
+9: 1001 = 1 + dp[9-8] = 2
+...
+"""
