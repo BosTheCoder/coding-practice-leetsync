093a9e857ae9e0e6bdf71b79789a3e011ea822c6
Bosire Nyakundi <25325707+BossCodersHQ@users.noreply.github.com>
2024-12-09 07:09:09 +0000

    Time: 0 ms (100.00%) | Memory: 17.4 MB (5.17%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -1,8 +1,8 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        dp = deque([1,1])
+        dp = [1,1]
         for i in range(2, n+1):
             steps_required = dp[0] + dp[1]
-            dp.popleft()
-            dp.append(steps_required)
+            dp[0] = dp[1]
+            dp[1] = steps_required
         return dp[1]
