59bc661e916909677a299012f652d81b1ca53e5a
Bosire Nyakundi <25325707+BossCodersHQ@users.noreply.github.com>
2025-01-15 07:53:16 +0000

    Time: 102 ms (23.50%) | Memory: 30.2 MB (8.89%) - LeetSync

---
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.py
@@ -0,0 +1,34 @@
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        lowest = min(nums)
+        largest = max(nums)
+        size = largest - lowest + 1
+        arr = [None] * size
+        for num in nums:
+            ix = num - lowest
+            if arr[ix] is None:
+                arr[ix] = (num,0)
+            else:
+                _,curr = arr[ix]
+                arr[ix] = (num, curr+1)
+        
+        print(arr)
+
+        end = len(arr) -1
+        curr = None
+        while k > 0 and end >= 0:
+            if arr[end] is not None:
+                _, num_values = arr[end]
+                while num_values > 0 and k>0:
+                    k -= 1
+                    curr = arr[end]
+                    num_values -= 1
+                    # print(curr)
+                k -= 1
+                curr = arr[end]
+                # print(curr)
+            end -= 1
+        return curr[0]
+
+
+
