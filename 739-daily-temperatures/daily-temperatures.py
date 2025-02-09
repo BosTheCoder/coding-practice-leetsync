819495bfd58953a0d59a109af5f59b2f21177906
Bosire Nyakundi <25325707+BossCodersHQ@users.noreply.github.com>
2025-02-09 08:18:58 +0000

    Time: 114 ms (55.32%) | Memory: 33.7 MB (20.86%) - LeetSync

--- 739-daily-temperatures/daily-temperatures.py
+++ 739-daily-temperatures/daily-temperatures.py
@@ -1,18 +1,12 @@
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
-        ret = [float("inf")] * len(temperatures)
-        seen = {}
-        for i in range(len(temperatures)-1,-1,-1):
-            temp = temperatures[i]
-            for j in range(temp+1,101):
-                if j in seen:
-                    ret[i] = min(ret[i],seen[j] - i)
-            ret[i] = 0 if ret[i] == float("inf") else ret[i]
-            seen[temp] = i
+        stack = [] # [(75,2),(72,5)]
+        ret = [0]*len(temperatures) # [1,1,0,2,1,0,0,0]
+        for ix, temp in enumerate(temperatures): # 5,72
+            while stack and stack[-1][0] < temp:
+                old_temp, old_ix = stack.pop()  # 71,3
+                ret[old_ix] = ix - old_ix   # 2
+            stack.append((temp,ix))
         return ret
-                    
-"""
-            
-73,74,75,71,69,72,76-(76,0),73-(73,0)
 
-"""
+
