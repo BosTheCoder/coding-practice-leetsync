93c571670d6ef381cd50f4604dca9a6729d4e951
Bosire Nyakundi <25325707+BossCodersHQ@users.noreply.github.com>
2025-02-13 21:01:11 +0000

    Time: 48 ms (23.83%) | Memory: 17.9 MB (94.37%) - LeetSync

--- 133-clone-graph/clone-graph.py
+++ 133-clone-graph/clone-graph.py
@@ -9,15 +9,32 @@ class Node:
 from typing import Optional
 class Solution:
     def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
+        if node is None:
+            return node
+
+        q = Deque()
+        q.appendleft((node,None))
         seen = {}
 
-        def dfs(curr):
-            if curr.val in seen:
-                return seen[curr.val]
-            new_node = Node(curr.val)
-            seen[curr.val] = new_node
-            new_neighbors = [dfs(neighbor) for neighbor in curr.neighbors]
-            new_node.neighbors = new_neighbors
-            return new_node
-        
-        return dfs(node) if node else node
+        while q:
+            curr, parent = q.pop()
+
+            # copy node
+            if curr.val not in seen:
+                curr_copy = Node(curr.val)
+                seen[curr.val] = curr_copy
+            else:
+                curr_copy = seen[curr.val]
+            
+            # add chilrden to queue
+            for neighbor in curr.neighbors:
+                if neighbor.val not in seen:    
+                    q.appendleft((neighbor,curr_copy))
+
+            # do connection with parent
+            if parent:
+                curr_copy.neighbors.append(parent)
+                parent.neighbors.append(curr_copy)
+
+        return seen[node.val]
+
