b91444e39dc3663fba61b2699b5a40ce3d9a68ef
Bosire Nyakundi <25325707+BossCodersHQ@users.noreply.github.com>
2025-02-16 05:03:00 +0000

    Time: 0 ms (100.00%) | Memory: 18.7 MB (45.22%) - LeetSync

--- 704-binary-search/binary-search.py
+++ 704-binary-search/binary-search.py
@@ -1,16 +1,7 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        l,r = 0,len(nums)-1
-        while abs(l-r)>1:
-            mid = (l + r)//2
-            if target==nums[mid]:
-                return mid
-            if target>nums[mid]:
-                l = mid
-            else:
-                r = mid
-        if target == nums[l]:
-            return l
-        if target == nums[r]:
-            return r
-        return -1
+        x= bisect.bisect_right(nums, target)
+        # print(x)
+        if x-1>len(nums) or nums[x-1]!=target:
+            return -1
+        return x-1
