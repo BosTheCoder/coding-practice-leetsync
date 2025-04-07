f8e0d72aa0acbe129549fb476dd22a561306472b
Bosire Nyakundi <25325707+BossCodersHQ@users.noreply.github.com>
2025-04-07 13:06:04 +0100

    Time: 2059 ms (5.00%) | Memory: 21.9 MB (7.90%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -1,15 +1,25 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
+        nums.sort() # [-4,-1,-1,-1,0,1,2,2,2,3]
         ret = set()
-        nums.sort()
-        # print(nums)
-        for i, num in enumerate(nums):
-            # print(nums[i])
-            goal = 0 - num
-            needed = set()
-            for j in range (i+1, len(nums)):
-                if nums[j] in needed:
-                    # print(f"found {nums[i]} {goal-nums[j]} {nums[j]}")
-                    ret.add((nums[i], goal-nums[j], nums[j]))
-                needed.add(goal-nums[j])
-        return [list(s) for s in ret]
+        for i,num in enumerate(nums):   #1,-1
+            target = -num   # 1
+            hash = {}   # {2:-1, 1:0, }
+            curr = None
+            for j in range(i+1, len(nums)): # 6
+                curr = nums[j]  # 2
+                if curr in hash:   
+                    ret.add((num, hash[curr], curr))
+                else:
+                    needed = target - curr  # 
+                    hash[needed] = curr
+            
+        return [list(r) for r in ret]
+                    
+
+"""
+[-1,0,1,2,-1,-4, -1]
+
+
+
+"""
