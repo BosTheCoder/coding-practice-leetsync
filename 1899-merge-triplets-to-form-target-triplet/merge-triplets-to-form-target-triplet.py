82c840ec8413ec2cd6cc2de6b9b04f28857bdab9
Bosire Nyakundi <25325707+BossCodersHQ@users.noreply.github.com>
2025-04-16 05:18:12 +0100

    Time: 196 ms (5.04%) | Memory: 55.2 MB (32.81%) - LeetSync

---
+++ 1899-merge-triplets-to-form-target-triplet/merge-triplets-to-form-target-triplet.py
@@ -0,0 +1,20 @@
+class Solution:
+    def mergeTriplets(self, triplets: List[List[int]], target: List[int]) -> bool:
+        relevant = []
+        for triplet in triplets:
+            if not any([val>target[i] for i,val in enumerate(triplet)]):
+                relevant.append(triplet)
+
+        count = 0
+
+        print(relevant)
+        for i in range(3):
+            if any([triplet[i] == target[i] for triplet in relevant]):
+                count+=1
+
+        return count == 3
+"""
+1,2,5
+
+[[2,5,3],[2,3,4],[1,2,5],[5,2,3],[6,5,5]],
+"""
