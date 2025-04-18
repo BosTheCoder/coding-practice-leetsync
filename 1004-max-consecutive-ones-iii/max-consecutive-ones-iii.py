2670e60fe718de4025d6e0476ca286dbeb1d9f06
Bosire Nyakundi <25325707+BosTheCoder@users.noreply.github.com>
2025-04-18 11:42:06 +0100

    Time: 15 ms (99.39%) | Memory: 18.3 MB (42.45%) - LeetSync

--- 1004-max-consecutive-ones-iii/max-consecutive-ones-iii.py
+++ 1004-max-consecutive-ones-iii/max-consecutive-ones-iii.py
@@ -1,21 +1,17 @@
 class Solution:
     def longestOnes(self, nums: List[int], k: int) -> int:
-        max_window = 0
-        left = right = 0
-        num_zeros = 0
-        while left<=right and right < len(nums):
-            if nums[right] == 0:
-                num_zeros += 1
-            
-            while num_zeros>k and left<=right:
-                left+=1
-                if nums[left-1] == 0:
-                    num_zeros-=1
-            
-            right+=1
-            
-            max_window = max(max_window, right-left)
-        return max_window
+        left = 0
+        for right in range(len(nums)):
+            # If we included a zero in the window we reduce the value of k.
+            # Since k is the maximum zeros allowed in a window.
+            k -= 1 - nums[right]
+            # A negative k denotes we have consumed all allowed flips and window has
+            # more than allowed zeros, thus increment left pointer by 1 to keep the window size same.
+            if k < 0:
+                # If the left element to be thrown out is zero we increase k.
+                k += 1 - nums[left]
+                left += 1
+        return right - left + 1
                 
 
 
