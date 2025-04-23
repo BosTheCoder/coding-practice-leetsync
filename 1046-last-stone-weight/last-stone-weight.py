e0f135d0b9a3e5f3b1b1984336cf9adef62501dc
Bosire Nyakundi <25325707+BosTheCoder@users.noreply.github.com>
2025-04-23 20:24:04 +0100

    Time: 0 ms (100.00%) | Memory: 17.8 MB (51.37%) - LeetSync

--- 1046-last-stone-weight/last-stone-weight.py
+++ 1046-last-stone-weight/last-stone-weight.py
@@ -1,36 +1,16 @@
 class Solution:
     def lastStoneWeight(self, stones: List[int]) -> int:
-        def getlargeststones():
-            x = ix = y = iy = 0
-            for i, stone in enumerate(stones):
-                
-                if stone > y:
-                    # print("stone > y", stone, "i", i, "x",(x, ix), "y", (y,iy))
-                    x = y
-                    ix = iy
-                    y = stone
-                    iy = i
-                elif stone > x:
-                    # print("stone > x", stone, "i", i, "x",(x, ix), "y", (y,iy))
-                    x = stone
-                    ix = i
-            return x, ix, y, iy
-                
-        # print(stones)
-        while len(stones)>1:
-            x, ix, y, iy = getlargeststones()
-            if x == y:
-                # print(f"deletion x {x} ix {ix} y {y} iy {iy}")
-                del(stones[ix])
-                del(stones[iy])
-            else:
-                # print(f"chipped x {x} ix {ix} y {y} iy {iy}")
-                stones[iy] = y-x
-                del(stones[ix])
-            
-            # print(stones)
-                
-        if not stones:
-            return 0
-        else:
-            return stones[0]
+        heap = []
+        #need to make all stone weights negative to make it a min heap
+        for stone in stones:
+            heapq.heappush(heap,-stone)
+        
+        while len(heap) > 1:
+            stone1 = heapq.heappop(heap)
+            stone2 = heapq.heappop(heap)
+            if stone1 == stone2:
+                continue
+            new_stone = abs(stone1-stone2)
+            heapq.heappush(heap, -new_stone)
+        
+        return -heap[0] if heap else 0
