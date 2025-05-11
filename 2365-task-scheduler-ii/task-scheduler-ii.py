fead23b5e685e3b0390e3025fc56e4d5c28fe055
Bosire Nyakundi <25325707+BosTheCoder@users.noreply.github.com>
2025-05-11 10:01:43 +0100

    Time: 90 ms (77.52%) | Memory: 36.9 MB (44.19%) - LeetSync

--- 2365-task-scheduler-ii/task-scheduler-ii.py
+++ 2365-task-scheduler-ii/task-scheduler-ii.py
@@ -1,11 +1,10 @@
 import math
 class Solution:
     def taskSchedulerII(self, tasks: List[int], space: int) -> int:
-        count_dict = {}
-        total_days = 0
-        for task in tasks:
-            if task not in count_dict:
-                count_dict[task] = -math.inf
-            total_days = max(total_days + 1, count_dict[task] + space + 1)
-            count_dict[task] = total_days
-        return total_days
+        m, curr_day = {}, 0
+        for x in tasks:
+            curr_day += 1
+            if x in m and curr_day - m[x] <= space:
+                curr_day += space - (curr_day - m[x]) + 1
+            m[x] = curr_day
+        return curr_day
