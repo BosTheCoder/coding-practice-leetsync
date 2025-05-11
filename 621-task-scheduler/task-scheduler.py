8a814d3fed59e5e19f2ed77f122e48430e1d494f
Bosire Nyakundi <25325707+BosTheCoder@users.noreply.github.com>
2025-05-11 07:47:37 +0100

    Time: 259 ms (5.12%) | Memory: 19.2 MB (21.29%) - LeetSync

--- 621-task-scheduler/task-scheduler.py
+++ 621-task-scheduler/task-scheduler.py
@@ -7,24 +7,17 @@ class Solution:
             heapq.heappush(heap,(-count,char))
 
         times = {}
-        ret = []
-
         time = 0
         tasks_left = len(tasks)
+
         while tasks_left:
             for count, char in times.get(time, []):
                 heapq.heappush(heap, (-count, char))
             if time in times: del times[time]
 
-            # print(heap)
-
-            if len(heap) == 0:
-                ret.append(None)
-            else:
+            if len(heap)!=0:
                 count,char = heapq.heappop(heap)
                 count = abs(count)
-
-                ret.append(char)
                 tasks_left -= 1
                 count -= 1
                 next_time = time+n+1             
@@ -33,7 +26,6 @@ class Solution:
                 if count > 0:
                     times.setdefault(next_time,[]).append((count,char))
 
-
             time+=1
-        # print(ret)
+
         return time
