abae210d00d27ccad820b05aab8484ae80fba859
Bosire Nyakundi <25325707+BosTheCoder@users.noreply.github.com>
2025-05-13 18:11:44 +0100

    Time: 70 ms (38.85%) | Memory: 21.3 MB (33.79%) - LeetSync

--- 72-edit-distance/edit-distance.py
+++ 72-edit-distance/edit-distance.py
@@ -1,24 +1,16 @@
 class Solution:
     def minDistance(self, word1: str, word2: str) -> int:
-        if len(word1) > len(word2):
-            word1, word2 = word2, word1
-        width, height = len(word2)+1, len(word1)+1
-        memo = [[0]*width for _ in range(height)]
+        dp = [[float("inf")] * (len(word2) + 1) for i in range(len(word1) + 1)]
 
-        # fill in edges
-        for i in range(width-1):
-            memo[height-1][i]= width-1-i
-        
-        for j in range(height-1):
-            memo[j][width-1] = height-1-j
-        
-        # go right to left and fill in gaps
+        for j in range(len(word2) + 1):
+            dp[len(word1)][j] = len(word2) - j
+        for i in range(len(word1) + 1):
+            dp[i][len(word2)] = len(word1) - i
 
-        for j in range(height-2,-1,-1):
-            for i in range(width-2,-1,-1):
-                memo[j][i] = min(
-                    1 + memo[j+1][i+1],
-                    1 + memo[j+1][i],
-                    1 + memo[j][i+1],
-                )  if word1[j] != word2[i] else memo[j+1][i+1]
-        return memo[0][0]
+        for i in range(len(word1) - 1, -1, -1):
+            for j in range(len(word2) - 1, -1, -1):
+                if word1[i] == word2[j]:
+                    dp[i][j] = dp[i + 1][j + 1]
+                else:
+                    dp[i][j] = 1 + min(dp[i + 1][j], dp[i][j + 1], dp[i + 1][j + 1])
+        return dp[0][0]
