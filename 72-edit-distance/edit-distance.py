94dd09e6dcdbdc91282ee732b577d974fb73fce0
Bosire Nyakundi <25325707+BosTheCoder@users.noreply.github.com>
2025-05-13 18:03:10 +0100

    Time: 94 ms (8.23%) | Memory: 21.2 MB (33.79%) - LeetSync

---
+++ 72-edit-distance/edit-distance.py
@@ -0,0 +1,27 @@
+class Solution:
+    def minDistance(self, word1: str, word2: str) -> int:
+        width, height = len(word2)+1, len(word1)+1
+        memo = [[0]*width for _ in range(height)]
+
+        # fill in edges
+        for i in range(width-1):
+            memo[height-1][i]= width-1-i
+        
+        for j in range(height-1):
+            memo[j][width-1] = height-1-j
+        
+        # go right to left and fill in gaps
+
+        for j in range(height-2,-1,-1):
+            for i in range(width-2,-1,-1):
+                tmp = float("inf")
+                if word1[j] == word2[i]:
+                    tmp = memo[j+1][i+1]
+
+                memo[j][i] = min(
+                    1 + memo[j+1][i+1],
+                    1 + memo[j+1][i],
+                    1 + memo[j][i+1],
+                    tmp
+                )
+        return memo[0][0]
