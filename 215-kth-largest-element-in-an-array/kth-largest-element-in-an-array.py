ecfcf640e1648a5f7d46b64a17a34e00609bd3bd
Bosire Nyakundi <25325707+BosTheCoder@users.noreply.github.com>
2025-05-26 14:11:22 +0100

    Time: 95 ms (47.10%) | Memory: 28.8 MB (25.87%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.py
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.py
@@ -1,23 +1,39 @@
-class Solution:
-    def findKthLargest(self, nums, k):
-        def quick_select(nums, k):
-            pivot = random.choice(nums)
-            left, mid, right = [], [], []
+import random
+from typing import List
 
-            for num in nums:
-                if num > pivot:
-                    left.append(num)
-                elif num < pivot:
-                    right.append(num)
-                else:
-                    mid.append(num)
-            
-            if k <= len(left):
-                return quick_select(left, k)
-            
-            if len(left) + len(mid) < k:
-                return quick_select(right, k - len(left) - len(mid))
-            
-            return pivot
-        
-        return quick_select(nums, k)
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        # we want the element whose rank in *descending* order is k-1
+        target = k - 1
+        lo, hi = 0, len(nums) - 1
+
+        while True:
+            # 1. choose a random pivot, swap it to the front for convenience
+            pivot_idx = random.randint(lo, hi)
+            pivot = nums[pivot_idx]
+            nums[pivot_idx], nums[lo] = nums[lo], nums[pivot_idx]
+
+            # 2. 3-way in-place partition:   >pivot | =pivot | <pivot
+            gt, i, lt = lo, lo, hi                     # gt: next slot for >pivot
+            while i <= lt:
+                if nums[i] > pivot:                    # belongs in “greater”
+                    nums[i], nums[gt] = nums[gt], nums[i]
+                    gt += 1
+                    i += 1
+                elif nums[i] < pivot:                  # belongs in “less”
+                    nums[i], nums[lt] = nums[lt], nums[i]
+                    lt -= 1
+                else:                                  # equal to pivot
+                    i += 1
+
+            # array now looks like:  [lo .. gt-1]  >p  |  [gt .. lt]  =p  |  [lt+1 .. hi]  <p
+            left_size   = gt - lo          # number of >pivot elements
+            mid_size    = lt - gt + 1      # number of =pivot elements
+
+            if target < left_size:         # k-th largest is in the “greater” block
+                hi = gt - 1
+            elif target >= left_size + mid_size:
+                lo = lt + 1
+                target -= left_size + mid_size
+            else:                          # inside the “equal” block → found it
+                return pivot
