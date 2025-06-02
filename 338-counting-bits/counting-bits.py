63d7ef24cc159ebab8793a379f5c6d268fcd4e7d
Bosire Nyakundi <25325707+BosTheCoder@users.noreply.github.com>
2025-06-02 18:57:13 +0100

    Time: 3 ms (94.52%) | Memory: 18.6 MB (59.04%) - LeetSync

--- 338-counting-bits/counting-bits.py
+++ 338-counting-bits/counting-bits.py
@@ -1,9 +1,15 @@
 class Solution:
     def countBits(self, n: int) -> List[int]:
-        mult = 1
+        mult = 2
+        count = 0
         ret = [0] * (n+1)
-        for i in range(1, n+1):
-            if mult * 2 == i:
-                mult *= 2
-            ret[i] = 1 + ret[i-mult]
+        for i in range(1,n+1):
+            if i == mult:
+                mult*=2
+                count =0
+        
+            ret[i] = 1 + ret[count]
+            count += 1
+            
+            
         return ret
