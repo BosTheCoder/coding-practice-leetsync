96bb025a90db3f2a195e3bdb72b2fce2dad5e1b1
Bosire Nyakundi <25325707+BosTheCoder@users.noreply.github.com>
2025-06-09 21:08:18 +0100

    Time: 97 ms (97.02%) | Memory: 40 MB (23.23%) - LeetSync

--- 295-find-median-from-data-stream/find-median-from-data-stream.py
+++ 295-find-median-from-data-stream/find-median-from-data-stream.py
@@ -1,22 +1,33 @@
 class MedianFinder:
 
     def __init__(self):
-        self.sorted_list = []
+        self.maxheap = []
+        self.minheap = []
 
     def addNum(self, num: int) -> None:
-        bisect.insort_right(self.sorted_list, num)
+        heapq.heappush(self.maxheap, -num)
+
+        if self.minheap and -self.maxheap[0] > self.minheap[0]:
+            max_element = -(heapq.heappop(self.maxheap))
+            heapq.heappush(self.minheap, max_element)
+
+        if len(self.maxheap) > len(self.minheap) + 1:
+            max_element = -(heapq.heappop(self.maxheap))
+            heapq.heappush(self.minheap, max_element)
+        elif len(self.minheap) > len(self.maxheap) + 1:
+            min_element = heapq.heappop(self.minheap)
+            heapq.heappush(self.maxheap, -min_element)
+
+            
 
     def findMedian(self) -> float:
-        length = len(self.sorted_list)
-        half = length/2
-        if length%2 == 0:
-            a = int(half)
-            b = a - 1
-            return (self.sorted_list[a] + self.sorted_list[b]) /2
+        total_length = len(self.maxheap) + len(self.minheap)
+        if total_length % 2 == 0:
+            median = (-self.maxheap[0] + self.minheap[0])/2
         else:
-            a = int(half)
-            return self.sorted_list[a]
-        
+            median = -(self.maxheap[0]) if len(self.maxheap) > len(self.minheap) else self.minheap[0]     
+
+        return median
 
 
 # Your MedianFinder object will be instantiated and called as such:
