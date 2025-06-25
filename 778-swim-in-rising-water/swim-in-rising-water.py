948569664fa26a83a06982917adbc1a72ada253e
Bosire Nyakundi <25325707+BosTheCoder@users.noreply.github.com>
2025-06-25 17:38:37 +0100

    Time: 22 ms (86.77%) | Memory: 18.4 MB (73.90%) - LeetSync

--- 778-swim-in-rising-water/swim-in-rising-water.py
+++ 778-swim-in-rising-water/swim-in-rising-water.py
@@ -1,41 +1,34 @@
 class Solution:
     def swimInWater(self, grid: List[List[int]]) -> int:
-        """
-        have a min heap which stores (time at this point, (x,y))
-        you have a while loop that 
-        1. pops from the min heap t and x,y
-        1.1 check if (X,y) is in seen hash table, if it is then skip
-        1.2. if it isn't then add it in there
-        2. if x,y is (n-1,n-1) end algo and return t
-        3. if not continue
-        4. adds all 4 adjacent squares to the heap with their correct times
-        
-
-        """
-
         heap = []
-        n = len(grid)
+        dirs = [(0,1),(1,0),(0,-1),(-1,0)]
+        height = width = len(grid)
+        heapq.heappush(heap, (grid[0][0],0,0)) # time, i, j
         seen = set()
-        coords= [(0,1),(1,0), (-1,0), (0,-1)]
-        heapq.heappush(heap, (grid[0][0], 0, 0))
+        seen.add((0,0))
+
+        count = 0
         while heap:
-            t, x , y = heapq.heappop(heap)
-            if x == n-1 and y == n-1:
+            count += 1
+            t, i, j = heapq.heappop(heap)
+            
+            if (i,j) == (width-1, height-1):
                 return t
             
-            if (x,y) in seen:
-                continue
-            else:
-                seen.add((x,y))
-            
-            for ax, ay in coords:
-                nx = x + ax
-                ny = y + ay
+            for di, dj in dirs:
+                ni,nj = i+di, j+dj
 
-                if nx >= n or nx < 0 or ny >= n or ny < 0 or (nx,ny) in seen:
+                if ni>=width or ni<0 or nj>=height or nj <0 or (ni,nj) in seen:
                     continue
                 
-                newt = grid[ny][nx]
-                heapq.heappush(heap, ((max(t, newt)), nx, ny))
-        
-        return False
+                val = (max(t, grid[nj][ni]), ni, nj)
+
+                if (ni,nj) == (width-1,height-1):
+                    return val[0]
+                seen.add((ni,nj))
+                heapq.heappush(
+                    heap, 
+                    val
+                )
+        return None
+            
