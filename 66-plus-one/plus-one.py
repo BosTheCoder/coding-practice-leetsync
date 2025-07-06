9bcf3cdd686e2c25b0288fe28022aaaf03918c1a
Bosire Nyakundi <25325707+BosTheCoder@users.noreply.github.com>
2025-07-06 13:28:07 +0100

    Time: 0 ms (100.00%) | Memory: 17.8 MB (30.28%) - LeetSync

--- 66-plus-one/plus-one.py
+++ 66-plus-one/plus-one.py
@@ -1,12 +1,17 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
         carry = 1
-        ix = -1
-        for i in range(len(digits)-1, -1, -1):
-            digit = digits[i] + carry
-            carry = digit // 10
-            digits[i] = digit % 10
-            if carry == 0:
-                return digits
-        digits = [carry] + digits
-        return digits
+        q = Deque()
+        for i in range(len(digits)-1,-1,-1):
+            digit = digits[i]
+            digit += carry
+
+            carry = max(digit - 9,0)
+            digit = digit % 10
+
+            q.appendleft(digit)
+        
+        if carry:
+            q.appendleft(carry)
+        
+        return list(q)
