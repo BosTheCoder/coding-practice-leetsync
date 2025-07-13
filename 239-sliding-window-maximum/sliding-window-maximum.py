136b0299cc0a1d58e176f4742711339f67958686
Bosire Nyakundi <25325707+BosTheCoder@users.noreply.github.com>
2025-07-13 15:19:50 +0100

    Time: 213 ms (24.77%) | Memory: 31.7 MB (48.56%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.py
+++ 239-sliding-window-maximum/sliding-window-maximum.py
@@ -1,7 +1,7 @@
 class Solution:
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
         q = Deque()
-        maxw = []
+        maxw = [0] * (len(nums)-k+1)
 
         for i,num in enumerate(nums):
             while q and q[0][1] <= i:
@@ -13,7 +13,7 @@ class Solution:
             q.append((num, i+k))
 
             if i >= k-1:
-                maxw.append(q[0][0])
+                maxw[i-k+1] = q[0][0]
         
         return maxw
 
