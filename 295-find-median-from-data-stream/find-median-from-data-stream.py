f260169c68872d8579b561284b2db52c0497be6d
Bosire Nyakundi <25325707+BosTheCoder@users.noreply.github.com>
2025-07-13 15:50:13 +0100

    Time: 93 ms (98.26%) | Memory: 39.7 MB (43.59%) - LeetSync

--- 295-find-median-from-data-stream/find-median-from-data-stream.py
+++ 295-find-median-from-data-stream/find-median-from-data-stream.py
@@ -1,30 +1,34 @@
 class MedianFinder:
 
     def __init__(self):
-        self.minheap = []
-        self.maxheap = []
-
-    def addNum(self, num: int) -> None:
-        heapq.heappush(self.maxheap,-num)
-
-        if self.minheap and -self.maxheap[0] > self.minheap[0]:
-            el = -heapq.heappop(self.maxheap)
-            heapq.heappush(self.minheap, el)
-
-        if len(self.minheap) - len(self.maxheap) > 1:
-            el = heapq.heappop(self.minheap)
-            heapq.heappush(self.maxheap, -el)
-        elif len(self.maxheap) - len(self.minheap) > 1:
-            el = -heapq.heappop(self.maxheap)
-            heapq.heappush(self.minheap, el)
+        self.maxh = [] # left
+        self.minh = [] # right
         
 
+    def addNum(self, num: int) -> None:
+        heapq.heappush(self.maxh, -num)
+
+        # check values
+        if self.minh and -self.maxh[0] > self.minh[0]:
+            val = -heapq.heappop(self.maxh)
+            heapq.heappush(self.minh, val)
+        
+        # reorganize lists:
+        if len(self.maxh) > len(self.minh) + 1:
+            val = -heapq.heappop(self.maxh)
+            heapq.heappush(self.minh, val)
+        elif len(self.minh) > len(self.maxh) + 1:
+            val = heapq.heappop(self.minh)
+            heapq.heappush(self.maxh, -val)
+
+
     def findMedian(self) -> float:
-        total = len(self.minheap) + len(self.maxheap)
-        if total % 2 == 0:
-            return (-self.maxheap[0] + self.minheap[0])/2
+        if len(self.maxh) == len(self.minh):
+            return (-self.maxh[0] + self.minh[0]) /2
+        elif len(self.maxh) > len(self.minh):
+            return -self.maxh[0]
         else:
-            return -self.maxheap[0] if len(self.maxheap)> len(self.minheap) else self.minheap[0]
+            return self.minh[0]
         
 
 
