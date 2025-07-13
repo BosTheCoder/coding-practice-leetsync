e4c039da060bd86095fabcb4885b02127d70f35f
Bosire Nyakundi <25325707+BosTheCoder@users.noreply.github.com>
2025-07-13 14:49:44 +0100

    Time: 3 ms (74.61%) | Memory: 17.9 MB (91.67%) - LeetSync

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.py
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.py
@@ -1,20 +1,15 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        dp = {}  # key=(i, buying) val=max_profit
+        # memoization arrays
+        b = [0,0]
+        s = 0
 
-        def dfs(i, buying):
-            if i >= len(prices):
-                return 0
-            if (i, buying) in dp:
-                return dp[(i, buying)]
+        for i in range(len(prices)-1,-1,-1):
+            price = prices[i]
+            
+            b_new = max(s - price,b[0])
+            s_new = max(b[1] +price, s)
 
-            cooldown = dfs(i + 1, buying)
-            if buying:
-                buy = dfs(i + 1, not buying) - prices[i]
-                dp[(i, buying)] = max(buy, cooldown)
-            else:
-                sell = dfs(i + 2, not buying) + prices[i]
-                dp[(i, buying)] = max(sell, cooldown)
-            return dp[(i, buying)]
-
-        return dfs(0, True)
+            b = [b_new, b[0]]
+            s = s_new
+        return b[0]
