892d774c4f8c9014c310ff3f8e6c579f837c9709
Bosire Nyakundi <25325707+BosTheCoder@users.noreply.github.com>
2025-07-29 18:45:02 +0100

    Time: 44 ms (45.91%) | Memory: 17.9 MB (96.81%) - LeetSync

--- 572-subtree-of-another-tree/subtree-of-another-tree.py
+++ 572-subtree-of-another-tree/subtree-of-another-tree.py
@@ -1,28 +1,28 @@
-
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-
 class Solution:
-    
     def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
-        if not subRoot:
-            return True
-        if not root:
+        if not root and subRoot:
             return False
+        return (
+            self.is_tree(root, subRoot)
+            or self.isSubtree(root.left, subRoot)
+            or self.isSubtree(root.right, subRoot)
+        )
 
-        if self.sameTree(root, subRoot):
+        
+    
+    def is_tree(self,a, b):
+        if a is None and b is None:
             return True
-        return (self.isSubtree(root.left, subRoot) or 
-               self.isSubtree(root.right, subRoot))
-
-    def sameTree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
-        if not root and not subRoot:
-            return True
-        if root and subRoot and root.val == subRoot.val:
-            return (self.sameTree(root.left, subRoot.left) and 
-                   self.sameTree(root.right, subRoot.right))
-        return False
+        if a is None or b is None:
+            return False
+        return (
+            a.val == b.val 
+            and self.is_tree(a.left, b.left) 
+            and self.is_tree(a.right, b.right)
+        )
