d832973c9b01c89668e2b0029ad7f06faf66ba68
Bosire Nyakundi <25325707+BosTheCoder@users.noreply.github.com>
2025-08-03 22:57:03 +0100

    Time: 67 ms (16.42%) | Memory: 22.2 MB (5.82%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.py
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.py
@@ -3,42 +3,35 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-
-class HeapNode(ListNode):
-    def __init__(self, node):
-        super().__init__(node.val, node.next)
-    
-    def __lt__(self, heap_node):
-        return self.val < heap_node.val
-
-
 class Solution:
+    class NodeWrapper:
+        def __init__(self, node: ListNode):
+            self.node = node
+        
+        def __lt__(self, other: 'NodeWrapper'):
+            return self.node.val < other.node.val
     
-
-
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
         heap = []
-        curr = dummy = ListNode()
-        
-        for node in lists:
-            if node is None:
-                continue
+        print("starting")
+        for list in lists:
             
-            heap_node = HeapNode(node)
-            heapq.heappush(heap, heap_node)
-        
+            curr = list
+            print(f"working on curr {curr}")
+            while curr:
+                node_wrapper = self.NodeWrapper(curr)
+                heapq.heappush(heap, node_wrapper)
+                curr = curr.next
+        print(heap)
+
+        head= ListNode()
+        curr = head
 
         while heap:
-            next = heapq.heappop(heap)
-
-            # add the nodes next value to the heap
-            if next.next is not None:
-                heapq.heappush(heap, HeapNode(next.next))
-            
-            # update pointers
-            curr.next = next
-            curr = next
+            node = heapq.heappop(heap).node
+            curr.next = node
+            curr = curr.next
         
-        return dummy.next
-
+        curr.next = None
         
+        return head.next
