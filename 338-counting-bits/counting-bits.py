11d161900c6f9f5b629a83d668db337bee3c98ea
Bosire Nyakundi <25325707+BosTheCoder@users.noreply.github.com>
2025-08-17 19:50:02 +0100

    Time: 3 ms (94.34%) | Memory: 18.5 MB (90.27%) - LeetSync

--- 338-counting-bits/counting-bits.py
+++ 338-counting-bits/counting-bits.py
@@ -1,31 +1,6 @@
 class Solution:
     def countBits(self, n: int) -> List[int]:
-        """
-        0 = 0
-        01 = 1
-        10 = 2
-        11 =
-
-        """
-        nums = [0] * (n+1)
-        for i in range(n+1):
-            s = str(bin(i))[2:]
-            num_ones = len([c for c in s if c == "1"])
-            nums[i] = num_ones
-        return nums
-
-
-"""
-0 0
-1 1
-2 10
-3 11
-4 100
-5 101
-6 110
-7 111
-8 1000
-9 1001
-
-
-"""
+        dp = [0] * (n + 1)
+        for i in range(n + 1):
+            dp[i] = dp[i >> 1] + (i & 1)
+        return dp
