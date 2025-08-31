251e25841873d7f492a03951da8b16a3ecb504cf
Bosire Nyakundi <25325707+BosTheCoder@users.noreply.github.com>
2025-09-01 00:03:17 +0100

    Time: 7 ms (45.25%) | Memory: 20.9 MB (48.72%) - LeetSync

--- 543-diameter-of-binary-tree/diameter-of-binary-tree.py
+++ 543-diameter-of-binary-tree/diameter-of-binary-tree.py
@@ -4,19 +4,19 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution:
-    def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
-        def dfs(node):
-            if not node:
-                return -1,-1
-            
-            left_with_node, left_without_node = dfs(node.left)
-            right_with_node, right_without_node = dfs(node.right)
-            
-            x=  max(left_with_node+1, right_with_node+1), max(left_without_node, right_without_node, left_with_node + right_with_node + 2)
-            # print(f"For node {node.val} we have {x}")
-            return x
+class Solution(object):
+    def diameterOfBinaryTree(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        self.ans = 0
         
-        a,b = dfs(root)
-        return max(a,b)
-        
+        def depth(p):
+            if not p: return 0
+            left, right = depth(p.left), depth(p.right)
+            self.ans = max(self.ans, left+right)
+            return 1 + max(left, right)
+            
+        depth(root)
+        return self.ans
