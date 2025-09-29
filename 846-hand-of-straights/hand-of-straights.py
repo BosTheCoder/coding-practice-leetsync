4d2bcd81e94c290c8a83d55a182d47fa32c5ef99
Bosire Nyakundi <25325707+BosTheCoder@users.noreply.github.com>
2025-09-29 19:33:26 +0100

    Time: 51 ms (23.39%) | Memory: 19.1 MB (44.71%) - LeetSync

--- 846-hand-of-straights/hand-of-straights.py
+++ 846-hand-of-straights/hand-of-straights.py
@@ -1,24 +1,29 @@
 class Solution:
     def isNStraightHand(self, hand: List[int], groupSize: int) -> bool:
-        if len(hand) % groupSize != 0:
-            return False
         counts = Counter(hand)
         heap = list(counts.keys())
         heapq.heapify(heap)
 
         while heap:
-            smallest = heap[0]
-
-            # try to create a group
+            val = heap[0]
             for i in range(groupSize):
-                curr = smallest + i
-                if curr in counts:
-                    counts[curr] -= 1
-                    if counts[curr] == 0:
-                        if curr != heap[0]:
-                            return False
+                curr = val + i
+                if curr not in counts: return False
+
+                counts[curr] -= 1
+                if counts[curr] == 0:
+                    del counts[curr]
+                    if heap[0] == curr:
                         heapq.heappop(heap)
-                else:
-                    return False
+                    else:
+                        return False
         return True
 
+"""
+
+{1:1, 2:2, 3:2, 6:1, 4:1, 7:1, 8:1}
+
+
+
+
+"""
