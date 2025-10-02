f6c12e79911391c4c7fddb8eb8e2fe7942a5b4d2
Bosire Nyakundi <25325707+BosTheCoder@users.noreply.github.com>
2025-10-03 00:55:40 +0100

    Time: 15 ms (14.51%) | Memory: 19.4 MB (50.47%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -1,9 +1,12 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
         m = (float("-inf"),0)
+        half = len(nums) // 2
         store = {}
         for num in nums:
             store[num] = store.get(num,0) + 1
+            if store[num] > half:
+                return num
             if m[0] < store[num]:
                 m = (store[num], num)
         
