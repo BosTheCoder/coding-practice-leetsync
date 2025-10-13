685f21a36c8bc0189ce0bd8f830cb924dc874955
Bosire Nyakundi <25325707+BosTheCoder@users.noreply.github.com>
2025-10-13 18:53:46 +0100

    Time: 0 ms (100.00%) | Memory: 18 MB (9.70%) - LeetSync

--- 1046-last-stone-weight/last-stone-weight.py
+++ 1046-last-stone-weight/last-stone-weight.py
@@ -2,16 +2,14 @@ class Solution:
     def lastStoneWeight(self, stones: List[int]) -> int:
         heap = [-stone for stone in stones]
         heapq.heapify(heap)
-        while len(heap)>1:
-            # get two heaviest stones
-            big = -heapq.heappop(heap)
-            small = -heapq.heappop(heap)
-            
-            # smash them togeher
-            leftover= big-small
 
-            # add back to heap if necessary
-            if leftover:
-                heapq.heappush(heap, -leftover)
-        
+        while len(heap)>1:
+            big, small = -heapq.heappop(heap), -heapq.heappop(heap)
+            if big == small:
+                continue
+
+            diff = big-small
+            heapq.heappush(heap,-diff)
         return -heap[0] if heap else 0
+
+        
