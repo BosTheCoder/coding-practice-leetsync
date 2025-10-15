5b4319618511156778d23600fdddb33f9dadc158
Bosire Nyakundi <25325707+BosTheCoder@users.noreply.github.com>
2025-10-15 23:18:48 +0100

    Time: 3 ms (40.93%) | Memory: 18 MB (18.12%) - LeetSync

--- 202-happy-number/happy-number.py
+++ 202-happy-number/happy-number.py
@@ -1,19 +1,17 @@
 class Solution:
     def isHappy(self, n: int) -> bool:
-        def getdigits(val:int) -> list:
-            ret = []
-            while val>=1:
-                ret.append(val % 10)
-                val = val//10
-            return ret
+        def squares(num):
+            digits = [int(d)*int(d) for d in list(str(num))]
+            return sum(digits)
         
-        cycleset = set()
-        while True:
-            if n == 1:
-                return True
-            if n in cycleset:
+        seen = set()
+        while n != 1:
+            if n in seen:
                 return False
-            cycleset.add(n)
-            digits = getdigits(n)
-            squared_digits = [digit**2 for digit in digits]
-            n = sum(squared_digits)
+            seen.add(n)
+            n = squares(n)
+        
+        return True
+
+
+        
