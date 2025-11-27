ffd6a8ccfcbf981bbef6c4d5676f89c64954d2a4
Bosire Nyakundi <25325707+BosTheCoder@users.noreply.github.com>
2025-11-27 08:24:04 +0000

    Time: 8 ms (32.19%) | Memory: 21.2 MB (80.27%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,19 +1,18 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        heap = []
-        counts = Counter(nums)
-        max_count = float("-inf")
-        for val, count in counts.items():
-            max_count = max(max_count, count)
+        c = Counter(nums)
+        size = max(c.values())
+        arr = [None] * (size+1)
+        for num, counts in c.items():
+            arr[counts] = [num] if not arr[counts] else arr[counts] + [num]
         
-        arr = [None] * (max_count+1)
-        for val, count in counts.items():
-            arr[count] = arr[count] + [val] if arr[count] else [val]
-
         ret = []
-        i = max_count
-        while len(ret) < k:
-            while arr[i] and len(ret) < k:
-                ret.append(arr[i].pop())
-            i -= 1
-        return ret
+        for i in range(size, -1, -1):
+            if not arr[i]: continue
+
+            for val in arr[i]:
+
+                ret.append(val)
+                if len(ret) >= k:
+                    return ret
+        
