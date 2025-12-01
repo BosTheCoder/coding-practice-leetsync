3c820a31818da7b62a5865c012a4c211ec4b02d2
Bosire Nyakundi <25325707+BosTheCoder@users.noreply.github.com>
2025-12-01 22:14:10 +0000

    Time: 19 ms (24.44%) | Memory: 20.5 MB (17.03%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.py
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.py
@@ -4,48 +4,46 @@
 #         self.val = val
 #         self.next = next
 
-class NodeWrapper:
+class Wrapper:
     def __init__(self, node: ListNode):
         self.node = node
     
-    def __lt__(self, node_wrapper):
-        return self.node.val < node_wrapper.node.val
+    def __lt__(self, other):
+        return self.node.val < other.node.val
 
-    def __repr__(self):
-        return f"NodeWrapper(val:{self.node.val})"
-    
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        
         heap = []
-        
-        # initialise heap with first node in each list
         for node in lists:
-            if not node:
-                continue
-            
-            # create wrapper and add to heap
-            node_wrapper = NodeWrapper(node)
-            heapq.heappush(heap, node_wrapper) 
+            if node:
+                heapq.heappush(heap, Wrapper(node))
         
-        # create final list iteratively
         dummy = ListNode()
         curr = dummy
+
         while heap:
-            node_wrapper = heapq.heappop(heap)
-            node = node_wrapper.node
+            smallest = heapq.heappop(heap).node  # 1
+            value_to_add = smallest.next    # 4
 
-            # after popping add the next node back on if there is one
-            if node.next:
-                node_wrapper = NodeWrapper(node.next)
-                heapq.heappush(heap, node_wrapper)
+            # Making sure to replenish the heap 
+            # We get the next one in the list
+            if value_to_add:
+                heapq.heappush(heap,Wrapper(value_to_add))
+
+            # Setting up the return list
+            curr.next = smallest            # 
+            curr = smallest                 # 
+
             
-            # Add original node to the list
-
-            curr.next = node
-            curr = curr.next
-        
         return dummy.next
-
-            
         
+        
+"""
+
+[[1,4,5],[1,3,4],[2,6]]
+
+heap = 2, 4, 3
+
+         c
+dummy -> 1 -> 1
+"""
