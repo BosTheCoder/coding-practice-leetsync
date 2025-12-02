1cae5395dfd8ba9708f769d56e93f1c8103e709c
Bosire Nyakundi <25325707+BosTheCoder@users.noreply.github.com>
2025-12-02 21:06:47 +0000

    Time: 585 ms (52.22%) | Memory: 20.7 MB (70.85%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -1,25 +1,39 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        nums.sort() # [-4,-1,-1,-1,0,1,2,2,2,3]
-        ret = set()
-        for i,num in enumerate(nums):   #1,-1
-            target = -num   # 1
-            hash = {}   # {2:-1, 1:0, }
-            curr = None
-            for j in range(i+1, len(nums)): # 6
-                curr = nums[j]  # 2
-                if curr in hash:   
-                    ret.add((num, hash[curr], curr))
+        nums.sort()
+        length = len(nums)-1
+        ret = []
+        for i, num in enumerate(nums):
+            # Skip the same i value to avoid duplicates
+            if i >= 1 and nums[i] == nums[i-1]:
+                continue
+
+            # Start two sum
+            left = i+1
+            right = length
+
+            while left < right:
+                total = nums[i] + nums[left] + nums[right]
+                if total < 0:
+                    left += 1
+                elif total == 0:
+                    ret.append([nums[i], nums[left], nums[right]])
+
+                    # Skip similar values
+                    val = nums[left]
+                    while nums[left] == val and left<right:
+                        left+=1
                 else:
-                    needed = target - curr  # 
-                    hash[needed] = curr
-            
-        return [list(r) for r in ret]
-                    
-
+                    right -= 1
+        return ret
 """
-[-1,0,1,2,-1,-4, -1]
+[-1,0,1,2,-1,-4]
+
+     i   l        r
+[-4, -1, -1, 0, 1, 2]
 
 
 
+     i   l                 r
+[-4, -3, -1, -1, -1, 0, 1, 4]
 """
