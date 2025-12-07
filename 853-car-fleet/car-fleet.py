070a8920d637cfb53ed379f1322b66055c761b66
Bosire Nyakundi <25325707+BosTheCoder@users.noreply.github.com>
2025-12-07 19:22:21 +0000

    Time: 154 ms (87.34%) | Memory: 39.2 MB (84.72%) - LeetSync

--- 853-car-fleet/car-fleet.py
+++ 853-car-fleet/car-fleet.py
@@ -1,19 +1,15 @@
 class Solution:
     def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
-        sps = sorted(zip(position,speed))  # sorted positions and speeds
-        # print(sps)
-        time = [0] * len(position)
-        for i,ps in enumerate(sps):
-            p, s = ps
-            time[i] = (target - p) / s
-        # print(time)
-        groups = 1
-        curr = time[-1]
-        # print("setting curr", curr)
-        for i in range(len(speed)-2, -1, -1):
-            if time[i] > curr:
-                groups+=1
-                curr = time[i]
-                # print("setting curr", curr)
+        vectors = list(zip(position,speed))
+        vectors.sort()
+        
+        curr_max = 0
+        groups = 0
+        for pos, speed in reversed(vectors):
+            time = (target-pos)/speed
+            
+            if time > curr_max:
+                curr_max = time
+                groups += 1
         
         return groups
