6fc1aa06766abdf66d49e4f710f355bf1cb439ed
Bosire Nyakundi <25325707+BosTheCoder@users.noreply.github.com>
2025-12-09 06:55:38 +0000

    Time: 153 ms (69.34%) | Memory: 19 MB (87.12%) - LeetSync

--- 875-koko-eating-bananas/koko-eating-bananas.py
+++ 875-koko-eating-bananas/koko-eating-bananas.py
@@ -1,37 +1,22 @@
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
-        def caneat(k) -> bool:
-            hours =0
-            for pile in piles:
-                hours += pile//k +1 if pile%k !=0 else pile//k
-            return hours <= h
-        
-        left, right = 1, max(piles)+1
 
-        while right - left > 1:
-            mid = (right + left) // 2
-            cn = caneat(mid)
-            # print(mid, cn)
-            if not cn:
-                left = mid
-            else:
-                right = mid
-            # print("left", left, "right", right)
-        
-        return left if caneat(left) else right
+        def valid(k):
+            if k ==0:
+                return k 
+            total = 0
+            for pile in piles:
+                total += math.ceil(pile/k)
+            return total <= h
             
 
-"""
-k < max(largest pile)
-
-can do binary search between 0 & k?
-
-to figure out if a value k can do the job you can go through each value in the array
-
-val // k + 1 if val%k !=0 else val//k
-
-time complexity
-
-each time in binary search need to do p operations
-binary search is plogp so maybe plogp^2
-"""
+        l = 0
+        r = max(piles)
+        while l < r:
+            mid = l + (r-l)//2
+            if valid(mid):
+                r = mid
+            else:
+                l = mid + 1
+        
+        return r
