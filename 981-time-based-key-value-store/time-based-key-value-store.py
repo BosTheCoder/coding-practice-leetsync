40d7306a9425ec9603d0ff7410a91b0c470a2bbb
Bosire Nyakundi <25325707+BosTheCoder@users.noreply.github.com>
2025-12-14 09:24:08 +0000

    Time: 91 ms (79.56%) | Memory: 74.6 MB (52.28%) - LeetSync

--- 981-time-based-key-value-store/time-based-key-value-store.py
+++ 981-time-based-key-value-store/time-based-key-value-store.py
@@ -4,25 +4,16 @@ class TimeMap:
         self.d = defaultdict(list)
 
     def set(self, key: str, value: str, timestamp: int) -> None:
-        self.d[key].append((value,timestamp))
+        self.d[key].append((timestamp,value))
 
     def get(self, key: str, timestamp: int) -> str:
         arr = self.d[key]
         
-        l=0
-        r=len(arr)-1
-        while l<=r:
-            mid = l + (r-l)//2
+        ix = bisect.bisect_right(arr,timestamp, key=lambda x:x[0])
 
-            mv, mt = arr[mid]
-            if mt == timestamp:
-                return mv
-            elif mt < timestamp:
-                l = mid + 1
-            else:
-                r = mid -1
-        
-        return arr[r][0] if r>=0 else ""
+        if ix == 0:
+            return ""
+        return arr[ix-1][1]
 
 
 # Your TimeMap object will be instantiated and called as such:
