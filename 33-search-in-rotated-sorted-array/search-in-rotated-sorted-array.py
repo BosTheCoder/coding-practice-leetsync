c90dc4a9d290ec2a20f696e23f301f176f732f99
Bosire Nyakundi <25325707+BosTheCoder@users.noreply.github.com>
2026-01-20 07:38:02 +0000

    Time: 0 ms (100.00%) | Memory: 19.6 MB (9.85%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
@@ -1,21 +1,38 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        l, r = 0, len(nums) - 1
+        l =0
+        r = len(nums)
 
-        while l <= r:
-            mid = (l + r) // 2
-            if target == nums[mid]:
-                return mid
-
-            if nums[l] <= nums[mid]:
-                if target > nums[mid] or target < nums[l]:
-                    l = mid + 1
-                else:
-                    r = mid - 1
-
+        while l<r:
+            m = l + (r-l)//2
+            if nums[m]<=nums[-1]:
+                r = m
             else:
-                if target < nums[mid] or target > nums[r]:
-                    r = mid - 1
-                else:
-                    l = mid + 1
-        return -1
+                l = m+1
+        print(f"pivot is {l}")
+
+        # If pivot is 0 just treat it as regular binary search
+        if l == 0:
+            x = bisect_left(nums,target)
+            return x if x<len(nums) and nums[x]==target else -1
+
+        # Figuring out which half its' in
+        if nums[0] <= target <= nums[l-1]:
+            x = bisect_left(nums,target,0,l)
+            return x if nums[x]==target else -1
+        elif nums[l] <= target <= nums[-1]:
+            x = bisect_left(nums,target,l,len(nums))
+            return x if nums[x]==target else -1
+        else:
+            return -1
+
+"""
+
+4  l 
+5
+6
+-1 m
+0
+1 
+2  r
+"""
