1bfa6d8c0f3ef290ee53a9f2a5465f37186402a8
Bosire Nyakundi <25325707+BosTheCoder@users.noreply.github.com>
2026-01-21 21:08:59 +0000

    Time: 161 ms (55.43%) | Memory: 20.7 MB (13.99%) - LeetSync

--- 875-koko-eating-bananas/koko-eating-bananas.py
+++ 875-koko-eating-bananas/koko-eating-bananas.py
@@ -1,22 +1,35 @@
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
-
-        def valid(k):
-            if k ==0:
-                return k 
-            total = 0
-            for pile in piles:
-                total += math.ceil(pile/k)
-            return total <= h
-            
-
-        l = 0
-        r = max(piles)
-        while l < r:
-            mid = l + (r-l)//2
-            if valid(mid):
-                r = mid
-            else:
-                l = mid + 1
         
-        return r
+        def p(k):
+            time = 0
+            for pile in piles:
+                time += math.ceil(pile/k)
+            return time<=h
+        
+        max_k = max(piles)
+        r = max_k + 1
+        l = 1
+
+        while r>l:
+            mid = l+ (r-l)//2
+
+            if p(mid):
+                r=mid
+            else:
+                l=mid+1
+        
+        return l
+
+
+
+"""
+what p makes results space monotionic
+
+p is whether can eat the banaas or not
+
+
+def p(k):
+    
+
+"""
