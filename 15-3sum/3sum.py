96392f32eef323f656bb07d4f0b4ee5bc1459600
Bosire Nyakundi <25325707+BosTheCoder@users.noreply.github.com>
2026-01-26 06:53:48 +0000

    Time: 544 ms (73.02%) | Memory: 22.5 MB (7.90%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -1,25 +1,51 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         nums.sort()
-        print(nums)
         ret = []
-        for i, num in enumerate(nums):
-            if i and num == nums[i-1]:
+        for i in range(len(nums)):
+            if i>0 and nums[i]==nums[i-1]:
                 continue
-            target = 0 - num
+            
+            target = 0 - nums[i]
+            j = i+1
+            k = len(nums)-1
 
-            l, r = i+1, len(nums)-1
-            while l<r:
-                total = nums[l]+nums[r]
-                if total > target:
-                    r-=1
-                elif total < target:
-                    l += 1
+            while j<k:
+                total = nums[j] + nums[k]
+
+                if total == target:
+                    ret.append([nums[i],nums[j],nums[k]])
+                    j += 1
+                    while j<k and nums[j]==nums[j-1]:
+                        j+=1
+                elif total>target:
+                    k-=1
                 else:
-                    ret.append([num,nums[l],nums[r]])
-                    r-=1
-                    l+=1
-                    while nums[l] == nums[l-1] and l <r:
-                        l+=1
-                    
+                    j+=1
+        
         return ret
+
+
+"""
+
+-4 
+-1 
+-1 i
+-1 j
+0 
+1 
+2  
+
+
+
+
+if nums[i] + nums[k] < target:
+
+loop while j<k
+
+avoiding duplicates:
+- when you find a match move j forward to a number thats ot the same
+- move forward i to a number thats not the same
+"""
+
+
