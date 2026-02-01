4fea0d5f4703971429113135451deeb744eadf03
Bosire Nyakundi <25325707+BosTheCoder@users.noreply.github.com>
2026-02-01 08:50:03 +0000

    Time: 26 ms (99.16%) | Memory: 30.9 MB (35.72%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.py
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.py
@@ -1,24 +1,16 @@
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
-        def dfs(arr:list, v):
-            pivot = random.choice(arr)
-            # print(arr, "k",v, "pivot", pivot)
-            greater, equal, smaller = [], [], []
-            for num in arr:
-                if num >pivot:
-                    greater.append(num)
-                elif num == pivot:
-                    equal.append(num)
-                else:
-                    smaller.append(num)
-            # print(greater, equal, smaller)
-            if v <= len(greater):
-                return dfs(greater, v)
-            elif v > len(greater) + len(equal):
-                return dfs(smaller, v-len(greater)-len(equal))
-            else:
-                return equal[0]
-        return dfs(nums, k)
+        max_val = max(nums)
+        min_val = min(nums)
+        arr = [0] * (max_val - min_val + 1)
 
+        for num in nums:
+            arr[num-min_val] +=1
+        
 
+        for i in range(max_val-min_val, -1, -1):
+            k-=arr[i]
+            if k <=0:
+                return i+min_val
+        return 0
 
