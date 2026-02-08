2b7ba1dd998b73f51974a5bea2bba1357518d095
Bosire Nyakundi <25325707+BosTheCoder@users.noreply.github.com>
2026-02-08 14:41:21 +0000

    Time: 0 ms (100.00%) | Memory: 19.5 MB (52.76%) - LeetSync

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.py
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.py
@@ -1,25 +1,16 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        memo = {}
-        def dfs(i, buying):
-            if i >= len(prices):
-                return 0
-            if (i,buying) in memo:
-                return memo[(i,buying)]
+        prev_buy = 0
+        prev_sell = 0
+        prev_prev_buy = 0
 
-            if buying:
-                ret = max(
-                    dfs(i+1, not buying) - prices[i],
-                    dfs(i+1, buying)
-                )
-            else:
-                ret = max(
-                    dfs(i+2, not buying) + prices[i],
-                    dfs(i+1, buying)
-                )
-            
-            memo[(i,buying)] = ret
-            return ret
+        for i in range(len(prices)-1,-1,-1):
+            curr_buy = max(prev_sell - prices[i], prev_buy)
+            curr_sell = max(prev_sell, prev_prev_buy + prices[i])
 
+            # shift everything back
+            prev_prev_buy = prev_buy
+            prev_buy = curr_buy
+            prev_sell = curr_sell
         
-        return dfs(0,True)
+        return curr_buy
