f87211a4015edd43bb6d882e4c594657025f6de1
Bosire Nyakundi <25325707+BosTheCoder@users.noreply.github.com>
2026-02-08 14:21:48 +0000

    Time: 3 ms (32.03%) | Memory: 19.6 MB (23.08%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.py
+++ 22-generate-parentheses/generate-parentheses.py
@@ -1,25 +1,26 @@
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
         ret = []
-        def dfs(open, close, arr):
-            if close > open:
-                return 
-            if open == n and close == n:
+
+        def generate(open: int, closed: int, arr: list[str]):
+            if open==n and closed == n:
                 ret.append("".join(arr))
                 return
-            if open + close > 2 * n:
-                return 
-                
-            # add open bracket
-            arr.append("(")
-            dfs(open+1,close,arr)
-            arr.pop()
 
-            # Add close bracket
-            arr.append(")")
-            dfs(open, close+1, arr)
-            arr.pop()
+            if open < n:
+                arr.append("(")
+                open += 1
+                generate(open,closed, arr)
+                arr.pop()
+                open -= 1
 
 
-        dfs(0,0,[])
+            if closed < open:
+                arr.append(")")
+                closed += 1
+                generate(open, closed, arr)
+                arr.pop()
+                closed -= 1
+
+        generate(0,0,[])
         return ret
