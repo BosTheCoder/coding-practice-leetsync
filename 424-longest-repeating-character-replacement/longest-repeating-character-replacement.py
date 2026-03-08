49bc65e3277371f33e890a5497b590465c14c024
Bosire Nyakundi <25325707+BosTheCoder@users.noreply.github.com>
2026-03-08 16:40:12 +0000

    Time: 159 ms (20.03%) | Memory: 19.7 MB (82.83%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
@@ -1,21 +1,30 @@
+"""
+You are given a string s and an integer k. 
+You can choose any character of the string and change it to any other uppercase English character. 
+You can perform this operation at most k times.
+
+Return the length of the longest substring containing the same letter you can get after performing the above operations.
+
+
+APPLE
+k=2
+
+"""
+
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
-        l = 0
-        max_win = 0
-        d = {}
+        left = 0
+        counter = defaultdict(int)
+        max_length = 0
+        for right, c in enumerate(s):
+            counter[c]+=1
 
-        for r, c in enumerate(s):
-            d[c] = d.get(c,0) + 1
+            while (right-left+1) - max(counter.values()) > k:
+                counter[s[left]] -=1
+                left += 1
             
-            # shrink window if invalid
-            while (r - l + 1) - max(d.values()) > k:
-                d[s[l]] -= 1
-                l += 1
-            
-            # Check if window length is the largest
-            max_win = max(
-                max_win,
-                (r - l + 1)
-            )
+            max_length = max(max_length, right-left+1)
         
-        return max_win
+        return max_length
+            
+
