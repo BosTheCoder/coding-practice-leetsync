838b9e87543c0d5b6933f46def344f29d7155527
Bosire Nyakundi <25325707+BosTheCoder@users.noreply.github.com>
2026-03-08 15:57:17 +0000

    Time: 58 ms (68.21%) | Memory: 24.3 MB (87.77%) - LeetSync

--- 973-k-closest-points-to-origin/k-closest-points-to-origin.py
+++ 973-k-closest-points-to-origin/k-closest-points-to-origin.py
@@ -1,14 +1,23 @@
 class Solution:
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
-        # Max heap
-        distances = []
+        heap = []
 
-        for x,y in points:
-            distance = math.sqrt(x**2 + y**2)
-            heapq.heappush(distances,(-distance,[x,y]))
-            if len(distances) > k:
-                heapq.heappop(distances)
+        for ix,point in enumerate(points):
+            # print(point, (point[0]^2), (point[1]^2))
+            distance = sqrt((point[0] ** 2) + (point[1]**2))
 
-        return [d[1] for d in distances]
-    
+            if len(heap) <k:
+                heapq.heappush(heap, (-distance,ix))
+            else:
+                # we're at capacity, something has to go if we want to add
+                largest_dist_in_heap = -heap[0][0]
+                if distance < largest_dist_in_heap:
+                    heapq.heappush(heap,(-distance, ix))
+                    heapq.heappop(heap)
 
+        ret = []
+        for dist, ix in heap:
+            ret.append(points[ix])
+        
+        return ret
+                
