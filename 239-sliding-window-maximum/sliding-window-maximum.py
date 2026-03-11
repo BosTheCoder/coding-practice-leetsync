ae19d8c8200395d794e45340d1dba5069f161dcc
Bosire Nyakundi <25325707+BosTheCoder@users.noreply.github.com>
2026-03-11 07:32:06 +0000

    Time: 240 ms (19.45%) | Memory: 38.4 MB (12.08%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.py
+++ 239-sliding-window-maximum/sliding-window-maximum.py
@@ -1,26 +1,46 @@
 class Solution:
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
-        ret = []
-        i = j = 0
+        
         q = Deque()
+        l = 0
+        ret = []
+        for r,num in enumerate(nums):
+            # shorten window to what's valid
+            if r-l+1 > k:
+                l += 1
 
-        while j<len(nums):
-            # Shorten window
-            if j-i == k:
-                if q and q[0] == nums[i]:
-                    q.popleft()
-                i += 1
-
-            # Add new value
-            while q and nums[j] > q[-1]:
-                q.pop()
-            q.append(nums[j])
-
-            # Get max
-            if (j+1)-i == k:
-                ret.append(q[0])
+            # clear queue of old numbers (we should only need to pop one)
+            if q and q[0][1] < l:
+                q.popleft()
             
-            # move forward
-            j += 1
+
+            # handling the new number...
+            # if it's greater, we clear until we reach a num that's larger than it
+            # if it's less then we append
+            while q and num>q[-1][0]:
+                q.pop()
+            q.append((num,r))
+
+
+            # add to return array if length is long enough
+            if r-l+1 == k:
+                ret.append(q[0][0])
         
         return ret
+            
+
+
+"""
+q = (1,0) 
+window length = r-1 + 1 = 3
+
+1   l
+3   r
+-1 
+-3  r
+5
+3
+6
+7
+
+"""
