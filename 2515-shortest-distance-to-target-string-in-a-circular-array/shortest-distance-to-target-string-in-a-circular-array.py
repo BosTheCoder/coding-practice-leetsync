5de5602779e270ded7a3e6cdc3e0d46741e9ef57
Bosire Nyakundi <25325707+BosTheCoder@users.noreply.github.com>
2026-04-15 07:40:25 +0100

    Time: 3 ms (12.37%) | Memory: 19.5 MB (4.30%) - LeetSync

---
+++ 2515-shortest-distance-to-target-string-in-a-circular-array/shortest-distance-to-target-string-in-a-circular-array.py
@@ -0,0 +1,18 @@
+class Solution:
+    def closestTarget(self, words: List[str], target: str, startIndex: int) -> int:
+        targetIndices = [ix for ix,word in enumerate(words) if word==target]
+        if len(targetIndices) == 0:
+            return -1
+        
+        min_distance = float(inf)
+        for targetIndex in targetIndices:
+            # Calculate distance if going right from startindex
+            rightDistance = targetIndex - startIndex if targetIndex>=startIndex else (targetIndex+len(words))-startIndex
+            # Calculated distance if going left from startIndex
+            leftDistance = startIndex - targetIndex if startIndex>=targetIndex else startIndex - (targetIndex -len(words))
+
+            print(rightDistance, leftDistance)
+            min_distance = min(min_distance, leftDistance, rightDistance)
+
+        return min_distance
+
