6bdc12092b3f55f3c2762e2fdf736bfa4fa7f66d
Bosire Nyakundi <25325707+BosTheCoder@users.noreply.github.com>
2026-06-02 08:00:33 +0100

    Time: 3 ms (61.95%) | Memory: 19.5 MB (10.01%) - LeetSync

--- 1189-maximum-number-of-balloons/maximum-number-of-balloons.py
+++ 1189-maximum-number-of-balloons/maximum-number-of-balloons.py
@@ -1,13 +1,24 @@
 class Solution:
     def maxNumberOfBalloons(self, text: str) -> int:
-        ref_word = "balloon"
-        ref_counts = Counter(ref_word)
-        counts = Counter(text)
+        target_word = "balloon"
+        target_counter = Counter(target_word)
 
-        # adjust counts
-        for c, count in ref_counts.items():
-            counts[c] = counts.get(c,0)//count
+        curr_word = text
+        curr_counter = Counter(curr_word)
 
-        return min(counts.get(c,0) for c in ref_word)
+        num_instances = 0
+        while True:
+            for target_char, target_char_appearances in target_counter.items():
+                if target_char not in curr_counter:
+                    return num_instances
+                
+                curr_char_appearances = curr_counter.get(target_char)
+                curr_char_appearances -= target_char_appearances
 
-
+                if curr_char_appearances <0:
+                    return num_instances
+                
+                curr_counter[target_char] = curr_char_appearances
+            num_instances += 1
+        
+        return num_instances
