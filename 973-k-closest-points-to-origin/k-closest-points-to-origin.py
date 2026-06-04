2c05bbbd4ca8c4b4d97110db90adc0671e2e4f65
Bosire Nyakundi <25325707+BosTheCoder@users.noreply.github.com>
2026-06-04 07:43:15 +0100

    Time: 69 ms (41.87%) | Memory: 25.4 MB (23.53%) - LeetSync

--- 973-k-closest-points-to-origin/k-closest-points-to-origin.py
+++ 973-k-closest-points-to-origin/k-closest-points-to-origin.py
@@ -1,23 +1,16 @@
 class Solution:
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
-        heap = []
-
-        for ix,point in enumerate(points):
-            # print(point, (point[0]^2), (point[1]^2))
-            distance = sqrt((point[0] ** 2) + (point[1]**2))
-
-            if len(heap) <k:
-                heapq.heappush(heap, (-distance,ix))
-            else:
-                # we're at capacity, something has to go if we want to add
-                largest_dist_in_heap = -heap[0][0]
-                if distance < largest_dist_in_heap:
-                    heapq.heappush(heap,(-distance, ix))
-                    heapq.heappop(heap)
-
-        ret = []
-        for dist, ix in heap:
-            ret.append(points[ix])
         
-        return ret
-                
+        def distance(point: list[int]) -> int:
+            dist = math.sqrt(point[0]**2 + point[1]**2)
+            return dist
+        
+
+        heap = []
+        for point in points:
+            heapq.heappush(heap, (-distance(point), point[0], point[1]))
+            if len(heap) > k:
+                heapq.heappop(heap)
+            
+        
+        return [[x,y] for dist,x,y in heap]
