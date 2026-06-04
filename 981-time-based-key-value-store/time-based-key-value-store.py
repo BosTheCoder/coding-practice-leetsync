91c03835cf439bf316265bfd7754f40dce2483ec
Bosire Nyakundi <25325707+BosTheCoder@users.noreply.github.com>
2026-06-04 07:07:07 +0100

    Time: 140 ms (40.79%) | Memory: 69.2 MB (35.35%) - LeetSync

--- 981-time-based-key-value-store/time-based-key-value-store.py
+++ 981-time-based-key-value-store/time-based-key-value-store.py
@@ -1,32 +1,58 @@
 class TimeMap:
 
     def __init__(self):
-        self.d = defaultdict(list)
+        self.timemap_dict = {}
 
     def set(self, key: str, value: str, timestamp: int) -> None:
-        self.d[key].append((timestamp,value))
+        if key not in self.timemap_dict:
+            self.timemap_dict[key] = []
+
+        self.timemap_dict[key].append((timestamp,value))
+
 
     def get(self, key: str, timestamp: int) -> str:
-        
-        # binary search
-        arr = self.d[key]
-
-        if not arr:
+        ix = self.search(key, timestamp)
+        if ix <1:
             return ""
-        
+        else:
+            return self.timemap_dict[key][ix-1][1]
+    
+    
+    def search(self, key:str, timestamp: int) -> int:
+        """
+        Return the index of the first value
+        that was set at a timestamp larger than 
+        provided timestamp using binary search.
+        """
+        if key not in self.timemap_dict:
+            return 0
+        arr = self.timemap_dict[key]
         l = 0
         r = len(arr)
+
         while l<r:
-            mid = l+(r-l)//2
-            if arr[mid][0] >timestamp:
+            mid = l + (r-l)//2
+            mid_timestamp = arr[mid][0]
+            if mid_timestamp > timestamp:
                 r = mid
             else:
                 l = mid+1
+        
+        return l
+        
 
-        if l >0:
-            return arr[l-1][1]
-        else:
-            return ""
+"""
+0       T       T        T    = search(0)=0
+1       F       T        T    = search(1)=1 
+3       F       T        T    = search(1)=1
+4       F       F        T    = search(4)=2
+6       F       F        F    = search(6)=3
+7       F       F        F
+foo = [(1,bar),(4,bar2),(6,bar3)]
+
+
+"""
+        
 
 
 # Your TimeMap object will be instantiated and called as such:
