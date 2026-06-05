2a1b99fb3ebdc12ae7010f3ec5576fe4571b6731
Bosire Nyakundi <25325707+BosTheCoder@users.noreply.github.com>
2026-06-05 07:16:40 +0100

    Time: 59 ms (73.31%) | Memory: 30.9 MB (54.17%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.py
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.py
@@ -1,15 +1,49 @@
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
-        heap = []
+        arr = nums
+        while arr:
+            pivot = random.choice(arr)
+            print(pivot)
 
-        for num in nums:
-            if len(heap)<k:
-                heapq.heappush(heap, num)
-            else:
-                if num<=heap[0]:
-                    pass
+            left = []
+            middle = []
+            right = []
+            for num in arr:
+                if num<pivot:
+                    left.append(num)
+                elif num == pivot:
+                    middle.append(num)
                 else:
-                    heapq.heappush(heap, num)
-                    heapq.heappop(heap)
-        
-        return heap[0]
+                    right.append(num)
+            
+            print(left, middle, right)
+            if k<=len(right):
+                arr=right
+            elif k<=(len(right)+len(middle)):
+                return middle[0]
+            else:
+                arr=left
+                k = k - len(middle) - len(right)
+        raise ValueError()
+
+
+
+"""
+
+choose random number
+4
+split it
+
+left = 3,2,1    aka less than 
+middle = 4      aka equal to
+right = 5,6     aka greater than
+
+we know that 4 is the 3rd largest
+if k is < 3 we need to look in right
+if k is > 3 we need to look in left
+
+it's 1 indexed
+
+
+
+"""
