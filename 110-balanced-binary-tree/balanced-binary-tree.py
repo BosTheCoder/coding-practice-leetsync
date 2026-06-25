7c5a481e1883a248541d57a205425f84daac093e
Bosire Nyakundi <25325707+BosTheCoder@users.noreply.github.com>
2026-06-25 08:15:54 +0100

    Time: 0 ms (100.00%) | Memory: 20.6 MB (8.92%) - LeetSync

--- 110-balanced-binary-tree/balanced-binary-tree.py
+++ 110-balanced-binary-tree/balanced-binary-tree.py
@@ -7,24 +7,30 @@
 class Solution:
     def isBalanced(self, root: Optional[TreeNode]) -> bool:
         
+        def dfs(node) -> tuple[bool,int]:
+            """
+            Returns whether node is balanced and it's max height
+            """
 
-        def dfs(node):
             if not node:
-                return 0, True
+                return True,0
             
-            rh, rb = dfs(node.right)
-            if not rb:
-                return 0, False
+            left_balanced, left_height = dfs(node.left)
+            right_balanced, right_height = dfs(node.right)
 
-            lh, lb = dfs(node.left)
-            if not lb:
-                return 0, False
-
-            val = (
-                max(rh, lh) + 1,
-                abs(rh-lh) <= 1
+            return (
+                (left_balanced and right_balanced and abs(left_height-right_height)<=1),
+                max(left_height,right_height)+1
             )
-            return val
+
         
-        _, bal = dfs(root)
-        return bal
+
+        return dfs(root)[0]
+
+"""
+
+right and left subtrees of every node differ by at most one
+everychild node is balanced
+
+
+"""
